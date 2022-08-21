cbc6f6e32c29d89551f5098868ac05ab9340a19e
Nagib Alexandre <80482671+NagibAlexandre@users.noreply.github.com>
2022-08-21 17:09:28 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import *
+
+Site = Flask(__name__)
+
+@Site.route('/')
+def SiteIndex():
+    return render_template('index.html')
+
+# Esse código é para quando for rodar no Replit
+#app.run(host='0.0.0.0', debug=True)
+
+# Esse código é para quando for rodar em sua máquina
+Site.run(debug=True)
